5e7eebd47c048bf7a9fd71819465b95e12754fa7
mtbunrieuu <quandepzainhat718@gmail.com>
2025-06-01 19:10:13 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,13 +1,13 @@
-const url = 'https://raw.githubusercontent.com/mtbunrieuu/pizza-order-site/main/index.html';
+const url = "https://raw.githubusercontent.com/mtbunrieuu/pizza-order-site/main/index.html";
 
-addEventListener('fetch', event => {
+addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request) {
-  const response = await fetch(url);
-  const text = await response.text();
-  return new Response(text, {
-    headers: { 'Content-Type': 'text/html' },
+  const res = await fetch(url);
+  const html = await res.text();
+  return new Response(html, {
+    headers: { "content-type": "text/html" },
   });
 }
