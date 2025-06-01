f71d57d82daebfe988bda874e772906251ee1b29
mtbunrieuu <quandepzainhat718@gmail.com>
2025-06-01 19:32:05 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const url = "https://raw.githubusercontent.com/mtbunrieuu/pizza-order-site/main/index.html";
+const url = "https://raw.githubusercontent.com/mtbunrieuu/pizza-order-site/main/index.html?token=GHSAT0AAAAAADEOZS3FBESTA65FGFUM225C2B4JA3Q";
 
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
