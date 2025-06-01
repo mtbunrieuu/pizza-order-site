ca0f961732862336a00e85bf66f5867d09e6297b
mtbunrieuu <quandepzainhat718@gmail.com>
2025-06-01 19:39:35 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,13 +1,14 @@
-const url = "https://raw.githubusercontent.com/mtbunrieuu/pizza-order-site/main/index.html";
-
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
-});
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
 
 async function handleRequest(request) {
-  const res = await fetch(url);
-  const html = await res.text();
-  return new Response(html, {
-    headers: { "content-type": "text/html" },
-  });
+  const url = 'https://raw.githubusercontent.com/mtbunrieuu/pizza-order-site/main/index.html'
+  const resp = await fetch(url)
+  const text = await resp.text()
+  return new Response(text, {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8'
+    }
+  })
 }
